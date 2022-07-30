924976aeac42e2ec58a994fff2fd18ed668f48d7
WaweruMwangiD <wawerumwangi21@gmail.com>
2022-07-30 13:43:26 +0300

    Modified the app js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, useRoutes, Route} from 'react-router-dom';
 import './App.css';
 import Header from './Header';
+import Tindercards from './Tindercards';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <h1>Hello Dave</h1>
-        <Header />
+
+      <Header />
+      <Router>    
+        <useRoutes>
+          <Route path='/welcome'/>
+          <Route path='/'>
+            <h2>Hello</h2>
+          </Route>
+        </useRoutes>   
       </Router>
       
     </div>
